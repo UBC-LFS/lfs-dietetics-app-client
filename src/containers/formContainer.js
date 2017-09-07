ee58bda79a96667ea5d2bb74f624a3c3a07ed142
Patrick Lin <patricklin@alumni.ubc.ca>
2017-09-07 16:00:57 -0700

    empty-obj - formContainer

---
+++ src/containers/formContainer.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+export default class formContainer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            
+        }
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <h2>Welcome to React</h2>
+                </div>
+            <p className="App-intro">
+              To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+          </div>
+        );
+    }
+}
