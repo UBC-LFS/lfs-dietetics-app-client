ef286f0c7e2ebe26ed9b4b35011b081e98c8ea02
Patrick Lin <patricklin@alumni.ubc.ca>
2017-09-08 12:31:08 -0700

    rename / reformat form

--- src/containers/FormContainers.js
+++ src/containers/FormContainers.js
@@ -19,36 +19,36 @@ export default class formContainer extends Component {
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
-                     First Name: 
+                    First Name:
                         <input type="text" />
                 </label>
                 <label>
                     Last Name:
-                        <input type="text" /> 
+                        <input type="text" />
                 </label>
                 <label>
                     UBC Student Number:
-                        <input type="text" /> 
+                        <input type="text" />
                 </label>
                 <label>
                     Email Address:
-                        <input type="text" /> 
-                </label>  
+                        <input type="text" />
+                </label>
                 <label>
                     Phone Number:
-                        <input type="text" /> 
-                </label> 
+                        <input type="text" />
+                </label>
                 <label>
                     Is this your first application to the UBC Dietetics Major?:
                         <input type="radio" name="gender" value="male" checked /> Yes
-                        <input type="radio" name="gender" value="female"/> No, second
-                        <input type="radio" name="gender" value="other"/> Other (permission note is attached) 
-                </label>  
+                        <input type="radio" name="gender" value="female" /> No, second
+                        <input type="radio" name="gender" value="other" /> Other (permission note is attached)
+                </label>
                 <label>
                     Note: Students are only permitted to apply to the program two times, unless written permission is given via
-                    the 2018 Dietetics Admissions Special Permission Form (due November 15, 2016). If permission to re-apply has 
+                    the 2018 Dietetics Admissions Special Permission Form (due November 15, 2016). If permission to re-apply has
                     been granted, please include a copy of the signed form with your application).
-                </label> 
+                </label>
 
                 <input type="submit" value="Submit" />
             </form>
