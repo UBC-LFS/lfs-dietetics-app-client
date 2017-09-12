9d79e12d4439d1facd97ff93c3167269d0827fa9
Patrick Lin <patricklin@alumni.ubc.ca>
2017-09-12 16:45:15 -0700

    transport json data on submit

--- src/containers/FormContainer.js
+++ src/containers/FormContainer.js
@@ -3,23 +3,45 @@ import React, { Component } from 'react';
 export default class formContainer extends Component {
     constructor(props) {
         super(props)
-        this.state = {
-            email: '',
-            verifyEmail: '',
-        }
-
-        this.setEmail = this.setEmail.bind(this);
-        this.handleVerifyEmail = this.handleVerifyEmail.bind(this);
+        this.state = {}
         this.validateEmail = this.validateEmail.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    setEmail(event) {
-        this.setState({ email: event.target.value }, this.validateEmail)
+    setFirstName(event) {
+        this.setState({ firstName: event.target.value })
     }
 
-    handleVerifyEmail(event) {
-        this.setState({ verifyEmail: event.target.value }, this.validateEmail)
+    setLastName(event) {
+        this.setState({ lastName: event.target.value })
+    }
+
+    setUbcSN(event) {
+        this.setState({ ubcSN: event.target.value })
+    }
+
+    setEmail(event) {
+        this.setState({ email: event.target.value })
+    }
+
+    setVerifyEmail(event) {
+        this.setState({ verifyEmail: event.target.value })
+    }
+
+    setPhone(event) {
+        this.setState({ phone: event.target.value })
+    }
+
+    setApp(event) {
+        this.setState({ numOfApp: event.target.value })
+    }
+
+    setAboriginal(event) {
+        this.setState({ aboriginal: event.target.value })
+    }
+
+    setAborId(event) {
+        this.setState({ aborId: event.target.value })
     }
 
     validateEmail() {
@@ -37,14 +59,14 @@ export default class formContainer extends Component {
 
     handleSubmit(event) {
         event.preventDefault()
-        console.log(this.state.firstName)
+        console.log(this.state)
     }
 
     componentDidMount() {
-        fetch('http://localhost:8080/')
-            .then(response => response.json())
-            .then(x => console.log(x))
-            
+        //fetch('http://localhost:8080/')
+        //.then(response => response.json(s))
+        //.then(x => console.log(x))
+
     }
 
     render() {
@@ -54,37 +76,38 @@ export default class formContainer extends Component {
                 <div className="form">
                     <form onSubmit={this.handleSubmit}>
                         <div>
-                            <label>First Name: </label>
-                            <input type="text" id="firstName" value={this.state.firstName}/>
+                            <label>First Name:&nbsp;</label>
+                            <input type="text" id="firstName" value={this.state.firstName} onChange={this.setFirstName.bind(this)} />
                         </div>
                         <div>
-                            <label>Last Name:</label>
-                            <input type="text" id="lastName" />
+                            <label>Last Name:&nbsp;</label>
+                            <input type="text" id="lastName" value={this.state.lastName} onChange={this.setLastName.bind(this)} />
                         </div>
                         <div>
-                            <label>UBC Student Number:</label>
-                            <input type="number" id="UBC_SN" />
+                            <label>UBC Student Number:&nbsp;</label>
+                            <input type="number" id="ubcSN" value={this.state.ubcSN} onChange={this.setUbcSN.bind(this)} />
                         </div>
                         <div>
-                            <label>Email Address:</label>
-                            <input type="email" id="email1" value={this.state.email} onChange={this.setEmail} />
+                            <label>Email Address:&nbsp;</label>
+                            <input type="email" id="email" value={this.state.email} onChange={this.setEmail.bind(this)} />
                         </div>
                         <div>
-                            <label>Verify Email Address:</label>
-                            <input type="email" id="email2" value={this.state.verifyEmail} onChange={this.handleVerifyEmail} />
+                            <label>Verify Email Address:&nbsp;</label>
+                            <input type="email" id="verifyEmail" value={this.state.verifyEmail} onChange={this.setVerifyEmail.bind(this)} />
                             <div id='verifyEmail'> </div>
                         </div>
                         <div>
-                            <label>Phone Number:</label>
-                            <input type="number" id="phone" />
+                            <label>Phone Number:&nbsp;</label>
+                            <input type="number" id="phone" value={this.state.phone} onChange={this.setPhone.bind(this)} />
                         </div>
                         <div>
                             <label>Is this your first application to the UBC Dietetics Major?:</label>
                             <fieldset id="NumOfApps">
-                                <input type="radio" name="appNum" value="yes" checked /> Yes
-                                <input type="radio" name="appNum" value="no" /> No, second
-                                <input type="radio" name="appNum" value="other" /> Other (permission note is attached)
+                                <input type="radio" name="appNum" value="yes" checked={this.state.numOfApp === 'yes'} onChange={this.setApp.bind(this)} /> &nbsp; Yes &nbsp;&nbsp;
+                                <input type="radio" name="appNum" value="no" checked={this.state.numOfApp === 'no'} onChange={this.setApp.bind(this)} /> &nbsp; No, second &nbsp;&nbsp;
+                                <input type="radio" name="appNum" value="other" checked={this.state.numOfApp === 'other'} onChange={this.setApp.bind(this)} /> &nbsp; Other (permission note is attached) &nbsp;&nbsp;
                             </fieldset>
+                            <br />
                         </div>
                         <div>
                             <p><b>Note:</b> Students are only permitted to apply to the program two times, unless written permission is given via
@@ -104,21 +127,22 @@ export default class formContainer extends Component {
                             <div>
                                 <label>Do you identify yourself as an Aboriginal person of Canada?</label>
                                 <fieldset id="aboriginal">
-                                    <input type="radio" name="abor" value="yes" checked /> Yes
-                                    <input type="radio" name="abor" value="no" /> No
+                                    <input type="radio" name="abor" value="yes" checked={this.state.aboriginal === 'yes'} onChange={this.setAboriginal.bind(this)} /> &nbsp; Yes &nbsp;&nbsp;
+                                    <input type="radio" name="abor" value="no" checked={this.state.aboriginal === 'no'} onChange={this.setAboriginal.bind(this)} /> &nbsp; No &nbsp;&nbsp;
                                 </fieldset>
                             </div>
                             <div>
                                 <label>Do you identify with one or more of the following:</label>
                                 <fieldset id="aborId">
-                                    <input type="radio" name="idAbor" value="firstNation" checked /> First Nations
-                                    <input type="radio" name="idAbor" value="Metis" /> Métis
-                                    <input type="radio" name="idAbor" value="Inuit" /> Inuit
+                                    <input type="radio" name="idAbor" value="First Nation" checked={this.state.aborId === 'First Nation'} onChange={this.setAborId.bind(this)} /> &nbsp; First Nations &nbsp;&nbsp;
+                                    <input type="radio" name="idAbor" value="Métis" checked={this.state.aborId === 'Métis'} onChange={this.setAborId.bind(this)} /> &nbsp; Métis &nbsp;&nbsp;
+                                    <input type="radio" name="idAbor" value="Inuit" checked={this.state.aborId === 'Inuit'} onChange={this.setAborId.bind(this)} /> &nbsp; Inuit &nbsp;&nbsp;
                                 </fieldset>
                             </div>
                         </div>
                         <div>
-                            <input class="btn" type="submit" value="Submit" />
+                            <br />
+                            <input className="btn" type="submit" value="Submit" />
                         </div>
                     </form>
                 </div>
