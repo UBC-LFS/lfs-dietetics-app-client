05144e1cf761c5cf3215edbbe5fe2a66467e5e4e
Patrick Lin <patricklin@alumni.ubc.ca>
2017-09-26 14:02:20 -0700

    rm submit disable

--- src/containers/Controller.js
+++ src/containers/Controller.js
@@ -56,7 +56,7 @@ export default class AppContainer extends Component {
             errorList.push(' STUDENT NUMBER')
         }
         if (!this.validateNumber(state.phone)) {
-            errorList.push(' PHONE NUMBER')
+            errorList.push(' PHONE NUMBER SHOULD ONLY CONTAIN NUMBERS')
         }
         if (!this.validateEmail(state.email) || !this.validateEmail(state.verifyEmail)) {
             errorList.push(' INVALID EMAIL')
