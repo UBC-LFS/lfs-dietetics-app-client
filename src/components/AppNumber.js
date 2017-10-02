57bab06970dfb5c86909b1a2367669ca85ea01cb
Justin Lee <justinlee0022@gmail.com>
2017-10-02 11:26:05 -0700

    If applicationNumber exists show or else show nothing

--- src/components/AppNumber.js
+++ src/components/AppNumber.js
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 
 const AppNumber = (props) => {
     return (
-        <div className='box'>
-            <h1 style={{ textAlign: 'center' }}> Your Application Number is {props.applicationNumber}</h1>
-        </div>
+        props.applicationNumber ? 
+            <div className='box'>
+                <h1 style={{ textAlign: 'center' }}> Your Application Number is {props.applicationNumber}</h1>
+            </div>
+            : <div></div>
     )
 };
 
