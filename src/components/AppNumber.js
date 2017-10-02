d689f3dee7c68d7d8d86fcc49b6791ce05ad8d1d
Justin Lee <justinlee0022@gmail.com>
2017-10-02 13:23:14 -0700

    reverted appnumber display

--- src/components/AppNumber.js
+++ src/components/AppNumber.js
@@ -3,11 +3,11 @@ import PropTypes from 'prop-types';
 
 const AppNumber = (props) => {
     return (
-        props.applicationNumber ? 
+        // props.applicationNumber ? 
             <div className='box'>
                 <h1 style={{ textAlign: 'center' }}> Your Application Number is {props.applicationNumber}</h1>
             </div>
-            : <div></div>
+            // : <div></div>
     )
 };
 
