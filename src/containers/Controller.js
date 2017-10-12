86dc693e2e2847339d2022a4abf12cdabb866715
Patrick Lin <patricklin@alumni.ubc.ca>
2017-10-12 14:31:04 -0700

    rm patch from email

--- src/containers/Controller.js
+++ src/containers/Controller.js
@@ -140,7 +140,7 @@ export default class AppContainer extends Component {
           )
         }
         <div className='footer'>
-          <p>For assistance contact <a href='mailto:it@landfood.ubc.ca' target='_top' rel='noopener noreferrer'>it@landfood.ubc.ca</a></p>
+          <p>For assistance contact <a href='mailto:it@landfood.ubc.ca' target='_top'>it@landfood.ubc.ca</a></p>
           <a href='https://secure.landfood.ubc.ca/Shibboleth.sso/Logout?return=http://dietetics.landfood.ubc.ca' >CWL LOGOUT</a>
         </div>
       </div>
