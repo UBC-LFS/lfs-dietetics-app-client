fe487c2cd4aa24f1b8b1fda70083cba32db12347
Patrick Lin <patricklin@alumni.ubc.ca>
2017-10-12 13:57:51 -0700

    added contact information

--- src/containers/Controller.js
+++ src/containers/Controller.js
@@ -139,7 +139,8 @@ export default class AppContainer extends Component {
           <Form handleSubmit={this.handleSubmit.bind(this)} />
           )
         }
-        <div>
+        <div className='footer'>
+          <p>For assistance contact <a href='mailto:it@landfood.ubc.ca' target='_top' rel='noopener noreferrer'>it@landfood.ubc.ca</a></p>
           <a href='https://secure.landfood.ubc.ca/Shibboleth.sso/Logout?return=http://dietetics.landfood.ubc.ca' >CWL LOGOUT</a>
         </div>
       </div>
